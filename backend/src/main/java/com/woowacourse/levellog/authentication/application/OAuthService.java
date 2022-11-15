093b2b03cb6a10807fd9c55e671e5120029d9c50
SuyeonChoi <tndus970120@gmail.com>
2022-11-15 20:08:40 +0900

    test(OAuthService): 외부 API mocking

--- backend/src/main/java/com/woowacourse/levellog/authentication/application/OAuthService.java
+++ backend/src/main/java/com/woowacourse/levellog/authentication/application/OAuthService.java
@@ -1,8 +1,12 @@
 package com.woowacourse.levellog.authentication.application;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.woowacourse.levellog.authentication.domain.OAuthClient;
 import com.woowacourse.levellog.authentication.dto.request.GithubCodeRequest;
 import com.woowacourse.levellog.authentication.dto.response.GithubProfileResponse;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +15,22 @@ import org.springframework.stereotype.Service;
 public class OAuthService {
 
     private final OAuthClient oAuthClient;
+    private final ObjectMapper objectMapper;
 
     public GithubProfileResponse requestGithubProfile(final GithubCodeRequest request) {
-        final String code = request.getAuthorizationCode();
-        final String githubAccessToken = oAuthClient.getAccessToken(code);
-
-        return oAuthClient.getProfile(githubAccessToken);
+//        final String code = request.getAuthorizationCode();
+//        final String githubAccessToken = oAuthClient.getAccessToken(code);
+//        return oAuthClient.getProfile(githubAccessToken);
+        try {
+            Thread.sleep(500);
+            return objectMapper.readValue(request.getAuthorizationCode(),
+                    GithubProfileResponse.class);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (JsonMappingException e) {
+            throw new RuntimeException(e);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
